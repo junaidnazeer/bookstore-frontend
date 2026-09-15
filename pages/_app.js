5b17d69e57542858f12214ba507b4e0df179eb2a
Junaid Nazeer <junaidnazeer88@gmail.com>
2026-09-15 10:56:23 +0530

    Fix mobile viewport and flex overflow issues

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,16 @@
+import Head from "next/head";
 import "../styles/globals.css";
 import { CartProvider } from "../lib/cart-context";
 
 export default function App({ Component, pageProps }) {
   return (
     <CartProvider>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        />
+      </Head>
       <Component {...pageProps} />
     </CartProvider>
   );
